8928eee65c71a88e2f0dd58610010773268a23c7
nagashree7 <nagashree721@gmail.com>
2025-02-04 12:24:31 +0530

    commit

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "vm" {  
-         ami           = "ami-0550c2ee59485be53"  
+         ami           = "ami-0c614dee691cbbf37"  
          availability_zone = "us-east-1"
           instance_type = "t2.micro"   
           
