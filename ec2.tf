ea2ea63199a7186195878b3d0c537145c6ae5612
nagashree7 <nagashree721@gmail.com>
2025-02-04 14:16:51 +0530

    commit

--- ec2.tf
+++ ec2.tf
@@ -66,5 +66,5 @@ resource "aws_security_group" "web_sg" {
 
 # ✅ Output Instance Public IP
 output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.vm.public_ip
 }
